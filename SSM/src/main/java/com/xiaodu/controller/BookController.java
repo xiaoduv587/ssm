81a807b259c1d2b01bd1c5ec0d53f6be17b09d05
xiaoduv587 <m15701207383@163.com>
2020-08-14 15:13:23 +0800

    配置xml和Controller

---
+++ SSM/src/main/java/com/xiaodu/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.xiaodu.controller;
+
+import com.xiaodu.pojo.Books;
+import com.xiaodu.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @Autowired
+    @Qualifier("BookServiceImpl")
+    private BookService bookService;
+
+    @RequestMapping("/allBook")
+    public String list(Model model) {
+        List<Books> list = bookService.queryAllBook();
+        model.addAttribute("list", list);
+        return "allBook";
+    }
+
+}
