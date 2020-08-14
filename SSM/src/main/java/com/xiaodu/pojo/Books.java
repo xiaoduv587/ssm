08cfa31850a8b64d636e83296e7f1b821695dca3
xiaoduv587 <m15701207383@163.com>
2020-08-14 10:45:38 +0800

    添加资源文件，添加bean

---
+++ SSM/src/main/java/com/xiaodu/pojo/Books.java
@@ -0,0 +1,21 @@
+package com.xiaodu.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Books {
+
+    private int bookID;
+
+    private String bookName;
+
+    private int bookCounts;
+
+    private String detail;
+
+
+}
